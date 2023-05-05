c170ed61c2b746ca764b6300ad0e7c8b629f49ff
seon ji <wooj8072@gmail.com>
2023-05-05 23:51:36 +0900

    [add] 세슘 지도, 건물객체

---
+++ src/main/java/com/example/cesiumMap/CesiumMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.cesiumMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CesiumMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CesiumMapApplication.class, args);
+    }
+
+}
